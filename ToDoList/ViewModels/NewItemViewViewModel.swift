5068f14ac17849f123f903811e2b2e89a2c22a27
ewrika <egorkaomsk_2003@mail.ru>
2023-11-22 19:36:41 +0600

    сохранение дел в бд

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -4,7 +4,8 @@
 //
 //  Created by Георгий Борисов on 19.11.2023.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel:ObservableObject{
@@ -13,7 +14,26 @@ class NewItemViewViewModel:ObservableObject{
     @Published var showAlert = false
     init(){}
     
-    func save(){}
+    func save(){
+        guard canSave else{
+            return
+        }
+        //Get current id
+        guard let uId = Auth.auth().currentUser?.uid else{
+            return
+        }
+        //Create model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(id: newId, title: title, dueDate: dueDate.timeIntervalSince1970, createDate: Date().timeIntervalSince1970, isDone: false)
+        
+        //save model
+        let db = Firestore.firestore()
+        db.collection ("users")
+            .document(uId)
+            .collection("todos")
+            .document("newId")
+            .setData(newItem.asDictionary())
+    }
     
     
     var canSave: Bool {
